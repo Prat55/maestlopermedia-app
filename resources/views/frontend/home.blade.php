bb609657da6b5a6a802d1c63f780d3b8d9ef8fbd
Prat55 <pratikdesai9059@gmail.com>
2023-08-25 16:39:49 +0530

    changes

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -167,12 +167,15 @@
                         <div class="row">
                             <div class="col-lg-12 col-sm-6">
                                 <div class="service-item style-three wow fadeInRight delay-0-2s">
-                                    <div class="icon"><i class="flaticon-design"></i></div>
+
+                                    <div class="icon">
+                                        <i class="flaticon-web-programming"></i>
+                                    </div>
                                     <div class="content">
                                         <h4>
-                                            <a href="{{ route('services') }}#graphic">Graphic Designing</a>
+                                            <a href="{{ route('services') }}#website">Website Designing</a>
                                         </h4>
-                                        <a class="more-btn" href="{{ route('services') }}#graphic"><i
+                                        <a class="more-btn" href="{{ route('services') }}#website"><i
                                                 class="fal fa-long-arrow-right"></i></a>
                                     </div>
                                 </div>
@@ -195,32 +198,32 @@
                 <div class="col-lg-6">
                     <div class="feature-middle rmt-30">
                         <div class="image wow fadeInUp delay-0-2s">
-                            <img class="round" src="{{ asset('user-assets/images/features/feature-middle.png') }}"
+                            <img class="round" src="{{ asset('user-assets/images/services/service-section-man.jpg') }}"
                                 alt="Feature Middle" />
                         </div>
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="service-item style-three wow fadeInUp delay-0-3s">
-                                    <div class="icon"><i class="flaticon-online"></i></div>
+                                    <div class="icon">
+                                        <i class="flaticon-graphic-design"></i>
+                                    </div>
                                     <div class="content">
                                         <h4>
-                                            <a href="{{ route('services') }}#software">Software Development</a>
+                                            <a href="{{ route('services') }}#social">Social Media Marketing</a>
                                         </h4>
-                                        <a class="more-btn" href="{{ route('services') }}#software"><i
+                                        <a class="more-btn" href="{{ route('services') }}#social"><i
                                                 class="fal fa-long-arrow-right"></i></a>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="service-item style-three mt-30 wow fadeInUp delay-0-4s">
-                                    <div class="icon">
-                                        <i class="flaticon-web-programming"></i>
-                                    </div>
+                                    <div class="icon"><i class="flaticon-design"></i></div>
                                     <div class="content">
                                         <h4>
-                                            <a href="{{ route('services') }}#website">Website Designing</a>
+                                            <a href="{{ route('services') }}#graphic">Graphic Designing</a>
                                         </h4>
-                                        <a class="more-btn" href="{{ route('services') }}#website"><i
+                                        <a class="more-btn" href="{{ route('services') }}#graphic"><i
                                                 class="fal fa-long-arrow-right"></i></a>
                                     </div>
                                 </div>
@@ -233,16 +236,15 @@
                         <div class="row">
                             <div class="col-lg-12 col-sm-6">
                                 <div class="service-item style-three mt-100 wow fadeInLeft delay-0-2s">
-                                    <div class="icon">
-                                        <i class="flaticon-graphic-design"></i>
-                                    </div>
+                                    <div class="icon"><i class="flaticon-online"></i></div>
                                     <div class="content">
                                         <h4>
-                                            <a href="{{ route('services') }}#social">Social Media Marketing</a>
+                                            <a href="{{ route('services') }}#software">Software Development</a>
                                         </h4>
-                                        <a class="more-btn" href="{{ route('services') }}#social"><i
+                                        <a class="more-btn" href="{{ route('services') }}#software"><i
                                                 class="fal fa-long-arrow-right"></i></a>
                                     </div>
+
                                 </div>
                             </div>
                             <div class="col-lg-12 col-sm-6">
@@ -627,46 +629,42 @@
     {{-- * Brands Logo Section Start* --}}
     <section class="trigger section gutter-horizontal bg-gray gutter-vertical--m gutter-horizontal">
         <div class="customer-logos slider">
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/e4923bf90c04345e43aada42486ebc4f9a3e56eb/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f677265656e732d666f6f642d737570706c696572732e737667"
-                    alt="Kinderhotel Aschauerhof" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/1c89cd43ff20ebfdae6d7dfc615bed22897d4f2c/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f6175746f2d73706565642e737667"
-                    height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/e7141f1aa02b6721a702b44a3b14b7383e4539ed/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f63726f6674732d6163636f756e74616e74732e737667"
-                    height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/339edd4ba206d97f2bc7c03f7b7fd5a1b5c97111/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f63686573686972652d636f756e74792d68796769656e652d73657276696365732e737667"
-                    height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/b9d65aaf5e5d1769f8bb0e04247ff6cfa0efa2ab/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f666173742d62616e616e612e737667"
-                    height="150" width="150" alt="Tannenmuehle"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/8d4fa451b4f47d2d10ff585a78f7fbd88f8f5530/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f73706163652d637562652e737667"
-                    height="150" width="150" alt="Loeffele"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/1e0d6f19906c869766d638227e7e3936aa9295c7/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f6265617574792d626f782e737667"
-                    alt="Krone" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/0072b8f37157c7e0238342d8105dcc2c9b1e2217/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f626162792d7377696d2e737667"
-                    alt="Obereggen" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/6b4aa115c3423ecbad9da2dba885d2d43084acfa/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f7468652d64616e63652d73747564696f2e737667"
-                    alt="Ortnerhof" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/b0e5c1e174dcb2911bc712f240f7163fe597628c/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f6a616d65732d616e642d736f6e732e737667"
-                    alt="Ottonenhof" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/370291c50b74eeab6fe66ccc9e6ca410fc117ed9/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f7468652d7765622d776f726b732e737667"
-                    alt="Leiners" height="150" width="150"></a></div>
-            <div class="slide-in-right slide"><img
-                    src="https://camo.githubusercontent.com/0112d20015b4dad56fbb07088e76552042539151/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f70657465732d626c696e64732e737667"
-                    alt="Seitenalm" height="150" width="150"></a></div>
             <div class="slide-in-right slide">
-                <img src="https://camo.githubusercontent.com/e4eb289d352d7c4cbb8493d6a212448036e3e2d2/687474703a2f2f7069676d656e742e6769746875622e696f2f66616b652d6c6f676f732f6c6f676f732f766563746f722f636f6c6f722f796f67612d626162792e737667"
-                    alt="Testerhof" height="150" width="150">
-                </a>
+                <img src="{{ asset('user-assets/images/client2/1.png') }}" alt="Kinderhotel Aschauerhof">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/2.png') }}">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/3.png') }}">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/4.png') }}">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/5.svg') }}" alt="Tannenmuehle">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/6.png') }}" alt="Loeffele">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/7.png') }}" alt="Krone">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/8.png') }}" alt="Obereggen">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/9.png') }}" alt="Ortnerhof">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/10.png') }}" alt="Ottonenhof" height="150px"
+                    width="200px">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/11.png') }}" alt="Leiners">
+            </div>
+            <div class="slide-in-right slide">
+                <img src="{{ asset('user-assets/images/client2/12.png') }}" alt="Seitenalm">
             </div>
     </section>
     {{-- * Brands Logo Section End* --}}
